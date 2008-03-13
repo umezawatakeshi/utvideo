b2814903160b8023d0cbc29325ef6d9028ee0d03
UMEZAWA Takeshi <umezawa@utsoft.org>
2008-03-13 13:36:33 +0000

    utvideo のプロジェクトを追加した。

---
+++ utvideo/utvideo.cpp
@@ -0,0 +1,22 @@
+// utvideo.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+
+
+#ifdef _MANAGED
+#pragma managed(push, off)
+#endif
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
+#ifdef _MANAGED
+#pragma managed(pop)
+#endif
+
