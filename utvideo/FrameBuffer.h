a309bf01f5121bc7206c7c455e9f0960f111ed44
UMEZAWA Takeshi <umezawa@utsoft.org>
2008-03-18 18:57:07 +0000

    フレームバッファクラスを追加した。

---
+++ utvideo/FrameBuffer.h
@@ -0,0 +1,75 @@
+/* 文字コードはＳＪＩＳ 改行コードはＣＲＬＦ */
+/* $Id$ */
+/*
+ * Ut Video Codec Suite
+ * Copyright (C) 2008  UMEZAWA Takeshi
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * 
+ * Ut Video Codec Suite
+ * Copyright (C) 2008  梅澤 威志
+ * 
+ * このプログラムはフリーソフトウェアです。あなたはこれを、フリーソフ
+ * トウェア財団によって発行された GNU 一般公衆利用許諾契約書(バージョ
+ * ン2か、希望によってはそれ以降のバージョンのうちどれか)の定める条件
+ * の下で再頒布または改変することができます。
+ * 
+ * このプログラムは有用であることを願って頒布されますが、*全くの無保
+ * 証* です。商業可能性の保証や特定の目的への適合性は、言外に示された
+ * ものも含め全く存在しません。詳しくはGNU 一般公衆利用許諾契約書をご
+ * 覧ください。
+ * 
+ * あなたはこのプログラムと共に、GNU 一般公衆利用許諾契約書の複製物を
+ * 一部受け取ったはずです。もし受け取っていなければ、フリーソフトウェ
+ * ア財団まで請求してください(宛先は the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA)。
+ */
+
+#pragma once
+
+class CFrameBuffer
+{
+private:
+	void *m_pAllocatedAddr;
+	void *m_pBufferAddr;
+
+private:
+	CFrameBuffer(void);
+public:
+	~CFrameBuffer(void);
+
+private:
+	static CFrameBuffer *InternalNewBuffer(DWORD dwSize, DWORD dwMarginSize, DWORD flProtect);
+public:
+	static inline CFrameBuffer *NewBuffer(DWORD dwSize, DWORD dwMarginSize)
+	{
+		return InternalNewBuffer(dwSize, dwMarginSize, PAGE_READWRITE);
+	}
+	static inline const CFrameBuffer *NewZeroBuffer(DWORD dwSize, DWORD dwMarginSize)
+	{
+		return InternalNewBuffer(dwSize, dwMarginSize, PAGE_READONLY);
+	}
+
+public:
+	inline void *GetBuffer()
+	{
+		return m_pBufferAddr;
+	}
+	inline const void *GetBuffer() const
+	{
+		return m_pBufferAddr;
+	}
+};
