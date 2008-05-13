7849d3f0f9eea1ad7675410e6db6660521d8cb45
UMEZAWA Takeshi <umezawa@utsoft.org>
2008-05-13 13:01:28 +0000

    バージョンを 3.2.0 とする。

--- utvideo/utvideo.h
+++ utvideo/utvideo.h
@@ -48,7 +48,7 @@
 	(((DWORD)(fcc) & 0xff000000) >> 24))
 #endif
 
-#define UTVIDEO_ENCODER_VERSION 0x03010000 /* 3.1.0 */
+#define UTVIDEO_ENCODER_VERSION 0x03020000 /* 3.2.0 */
 
 extern HINSTANCE hModule;
 
