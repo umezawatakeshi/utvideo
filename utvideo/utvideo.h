88caffb402d135c92b242a11eb1784320452b0ef
UMEZAWA Takeshi <umezawa@utsoft.org>
2009-07-06 11:22:11 +0000

    バージョンを 5.2.3 とする。

--- utvideo/utvideo.h
+++ utvideo/utvideo.h
@@ -54,7 +54,7 @@
 	(BYTE)(fcc >> 16), \
 	(BYTE)(fcc >> 24)
 
-#define UTVIDEO_ENCODER_VERSION 0x05020200 /* 5.2.2 */
+#define UTVIDEO_ENCODER_VERSION 0x05020300 /* 5.2.3 */
 
 extern HINSTANCE hModule;
 
