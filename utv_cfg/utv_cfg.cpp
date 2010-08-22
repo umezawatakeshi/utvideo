ab9c7a308fe29870df8942204ea6a1bcb4986eed
UMEZAWA Takeshi <umezawa@utsoft.org>
2010-08-22 12:01:55 +0000

    コーデックのグローバルな設定を行うプログラムを追加した。

---
+++ utv_cfg/utv_cfg.cpp
@@ -0,0 +1,13 @@
+/* 文字コードはＳＪＩＳ 改行コードはＣＲＬＦ */
+/* $Id$ */
+
+// utv_cfg.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "utv_cfg.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
---
+++ utv_cfg/utv_cfg.h
@@ -0,0 +1,6 @@
+/* •¶šƒR[ƒh‚Í‚r‚i‚h‚r ‰üsƒR[ƒh‚Í‚b‚q‚k‚e */
+/* $Id$ */
+
+#pragma once
+
+#include "resource.h"
