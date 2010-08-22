866ebda67c14c3723db95fa783add024f8ff3136
UMEZAWA Takeshi <umezawa@utsoft.org>
2010-08-22 13:09:40 +0000

    コーデックのグローバルな設定を行うダイアログを追加した。

--- utv_cfg/resource.h
+++ utv_cfg/resource.h
@@ -2,10 +2,12 @@
 // Microsoft Visual C++ generated include file.
 // Used by utv_cfg.rc
 //
-#define IDC_MYICON                      2
 #define IDD_UTV_CFG_DIALOG              102
-#define IDD_ABOUTBOX                    103
 #define IDR_MAINFRAME                   128
+#define IDD_GLOBAL_CONFIG_DIALOG        129
+#define IDC_SAVE_CONFIG_CHECK           1001
+#define IDC_CHECK2                      1002
+#define IDC_IGNORE_SET_CONFIG_CHECK     1002
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -13,9 +15,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- utv_cfg/utv_cfg.cpp
+++ utv_cfg/utv_cfg.cpp
@@ -7,7 +7,72 @@
 #include "stdafx.h"
 #include "utv_cfg.h"
 
+INT_PTR CALLBACK DialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void OnInitDialog(HWND hWnd);
+void OnOK(HWND hWnd);
+
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
-	return 0;
+	return (int)DialogBox(hInstance, MAKEINTRESOURCE(IDD_GLOBAL_CONFIG_DIALOG), NULL, DialogProc);
+}
+
+INT_PTR CALLBACK DialogProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		OnInitDialog(hWnd);
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			OnOK(hWnd);
+			/* FALLTHROUGH */
+		case IDCANCEL:
+			EndDialog(hWnd, 0);
+			return TRUE;
+		}
+		break;
+	}
+
+	return FALSE;
+}
+
+void OnInitDialog(HWND hWnd)
+{
+	HKEY hkUtVideo;
+	DWORD dwValue;
+	DWORD cb;
+	DWORD dwType;
+
+	if (RegCreateKeyEx(HKEY_CURRENT_USER, "Software\\Ut Video Codec Suite", 0, NULL, 0, KEY_ALL_ACCESS, NULL, &hkUtVideo, NULL) != ERROR_SUCCESS)
+		return;
+
+	cb = sizeof(DWORD);
+	if (RegQueryValueEx(hkUtVideo, "SaveConfig", NULL, &dwType, (BYTE *)&dwValue, &cb) == ERROR_SUCCESS)
+		CheckDlgButton(hWnd, IDC_SAVE_CONFIG_CHECK, dwValue);
+
+	cb = sizeof(DWORD);
+	if (RegQueryValueEx(hkUtVideo, "IgnoreSetConfig", NULL, &dwType, (BYTE *)&dwValue, &cb) == ERROR_SUCCESS)
+		CheckDlgButton(hWnd, IDC_IGNORE_SET_CONFIG_CHECK, dwValue);
+
+	RegCloseKey(hkUtVideo);
+}
+
+void OnOK(HWND hWnd)
+{
+	HKEY hkUtVideo;
+	DWORD dwValue;
+
+	if (RegCreateKeyEx(HKEY_CURRENT_USER, "Software\\Ut Video Codec Suite", 0, NULL, 0, KEY_ALL_ACCESS, NULL, &hkUtVideo, NULL) != ERROR_SUCCESS)
+		return;
+
+	dwValue = IsDlgButtonChecked(hWnd, IDC_SAVE_CONFIG_CHECK);
+	RegSetValueEx(hkUtVideo, "SaveConfig", 0, REG_DWORD, (const BYTE *)&dwValue, sizeof(DWORD));
+
+	dwValue = IsDlgButtonChecked(hWnd, IDC_IGNORE_SET_CONFIG_CHECK);
+	RegSetValueEx(hkUtVideo, "IgnoreSetConfig", 0, REG_DWORD, (const BYTE *)&dwValue, sizeof(DWORD));
+
+	RegCloseKey(hkUtVideo);
 }
