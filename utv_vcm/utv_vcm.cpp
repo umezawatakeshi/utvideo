9798e03eec8c28f55f57280665869014a8f5a75f
UMEZAWA Takeshi <umezawa@utsoft.org>
2011-03-10 10:39:27 +0000

    - ファイルの先頭のコメントを追加した。 - DllMain() の書き方を調整した。

--- utv_vcm/utv_vcm.cpp
+++ utv_vcm/utv_vcm.cpp
@@ -1,3 +1,6 @@
+/* 文字コードはＳＪＩＳ 改行コードはＣＲＬＦ */
+/* $Id$ */
+
 // utv_vcm.cpp : DLL アプリケーションのエントリ ポイントを定義します。
 //
 
@@ -8,12 +11,9 @@
 #pragma managed(push, off)
 #endif
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
-    return TRUE;
+	return TRUE;
 }
 
 #ifdef _MANAGED
