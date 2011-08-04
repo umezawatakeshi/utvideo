3a36c888b044898224df6d0be7fe388f93b2a20f
UMEZAWA Takeshi <umezawa@utsoft.org>
2011-08-04 13:06:01 +0000

    バージョンを 9.0.3 とする。

--- utvideo/utvideo.h
+++ utvideo/utvideo.h
@@ -10,8 +10,8 @@
 	(BYTE)(fcc >> 24)
 
 #define UTVIDEO_VERSION_MASK                      0xffffff00
-#define UTVIDEO_VERSION                           0x09000100
-#define UTVIDEO_VERSION_STR                       "9.0.1"
+#define UTVIDEO_VERSION                           0x09000300
+#define UTVIDEO_VERSION_STR                       "9.0.3"
 
 /*
  * NOTE:
