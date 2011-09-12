bf4fa5989af5549383d907cff99fd268b77c5905
UMEZAWA Takeshi <umezawa@utsoft.org>
2011-09-12 12:07:06 +0000

    バージョンをバージョンを 10.0.1 とする。

--- utv_core/utvideo.h
+++ utv_core/utvideo.h
@@ -19,8 +19,8 @@
 
 
 #define UTVIDEO_VERSION_MASK                      0xffffff00
-#define UTVIDEO_VERSION                           0x0a000000
-#define UTVIDEO_VERSION_STR                       "10.0.0"
+#define UTVIDEO_VERSION                           0x0a000100
+#define UTVIDEO_VERSION_STR                       "10.0.1"
 
 /*
  * NOTE:
