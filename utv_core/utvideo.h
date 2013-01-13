87b26d839dd8e8b02fde7a2c8f11e8b71eaab4de
UMEZAWA Takeshi <umezawa@utsoft.org>
2013-01-13 14:13:08 +0000

    バージョンを 12.0.1 とする。

--- utv_core/utvideo.h
+++ utv_core/utvideo.h
@@ -26,8 +26,8 @@ typedef ptrdiff_t ssize_t;
 
 
 #define UTVIDEO_VERSION_MASK                      0xffffff00
-#define UTVIDEO_VERSION                           0x0c000000
-#define UTVIDEO_VERSION_STR                       "12.0.0"
+#define UTVIDEO_VERSION                           0x0c000100
+#define UTVIDEO_VERSION_STR                       "12.0.1"
 
 /*
  * NOTE:
