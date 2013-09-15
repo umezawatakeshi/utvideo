4332a887da5e82fa193547f99ed1f5c7dc5cf013
UMEZAWA Takeshi <umezawa@utsoft.org>
2013-09-15 10:56:10 +0000

    cpuid() や xgetbv() の結果をデバッグ出力する方法を変更して見やすくした。

--- utv_core/TunedFunc_x86x64.cpp
+++ utv_core/TunedFunc_x86x64.cpp
@@ -241,6 +241,30 @@ const TUNEDFUNC &tfnAVX2 = tfnSSE41;
 class CTunedFuncInitializer
 {
 public:
+	static inline void cpuid(cpuid_result *result, uint32_t leaf, uint32_t subleaf)
+	{
+		::cpuid(result, leaf, subleaf);
+#ifdef _DEBUG
+		char buf[16];
+		if (leaf >= 0x80000000)
+			sprintf(buf, "ex%d", leaf - 0x80000000);
+		else if (leaf == 7)
+			sprintf(buf, "7.%d", subleaf);
+		else
+			sprintf(buf, "%d", leaf);
+		_RPT4(_CRT_WARN, "CPUID.%-3s EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", buf,
+			result->eax, result->ebx, result->ecx, result->edx);
+#endif
+	}
+
+	static inline void xgetbv(xgetbv_result *result, uint32_t idx)
+	{
+		::xgetbv(result, idx);
+#ifdef _DEBUG
+		_RPT2(_CRT_WARN, "XGETBV.%-2d EAX=%08X EDX=%08X\n", result->eax, result->edx);
+#endif
+	}
+
 	CTunedFuncInitializer()
 	{
 		cpuid_result cpuid_0   = { 0, 0, 0, 0 };
@@ -256,7 +280,6 @@ public:
 		char procbrand[64];
 
 		cpuid(&cpuid_0, 0, 0);
-		_RPT4(_CRT_WARN, "CPUID.0   EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_0.eax, cpuid_0.ebx, cpuid_0.ecx, cpuid_0.edx);
 		*(uint32_t *)(vendor+0) = cpuid_0.ebx;
 		*(uint32_t *)(vendor+4) = cpuid_0.edx;
 		*(uint32_t *)(vendor+8) = cpuid_0.ecx;
@@ -264,22 +287,17 @@ public:
 		_RPT1(_CRT_WARN, "CPUID     vendor=\"%s\"\n", vendor);
 
 		cpuid(&cpuid_ex0, 0x80000000, 0);
-		_RPT4(_CRT_WARN, "CPUID.ex0 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_ex0.eax, cpuid_ex0.ebx, cpuid_ex0.ecx, cpuid_ex0.edx);
 
 		if (cpuid_ex0.eax >= 0x80000001)
 		{
 			cpuid(&cpuid_ex1, 0x80000001, 0);
-			_RPT4(_CRT_WARN, "CPUID.ex1 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_ex1.eax, cpuid_ex1.ebx, cpuid_ex1.ecx, cpuid_ex1.edx);
 		}
 
 		if (cpuid_ex0.eax >= 0x80000004)
 		{
 			cpuid(&cpuid_ex2, 0x80000002, 0);
-			_RPT4(_CRT_WARN, "CPUID.ex2 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_ex2.eax, cpuid_ex2.ebx, cpuid_ex2.ecx, cpuid_ex2.edx);
 			cpuid(&cpuid_ex3, 0x80000003, 0);
-			_RPT4(_CRT_WARN, "CPUID.ex3 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_ex3.eax, cpuid_ex3.ebx, cpuid_ex3.ecx, cpuid_ex3.edx);
 			cpuid(&cpuid_ex4, 0x80000004, 0);
-			_RPT4(_CRT_WARN, "CPUID.ex4 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_ex4.eax, cpuid_ex4.ebx, cpuid_ex4.ecx, cpuid_ex4.edx);
 			*(uint32_t *)(procbrand+ 0) = cpuid_ex2.eax;
 			*(uint32_t *)(procbrand+ 4) = cpuid_ex2.ebx;
 			*(uint32_t *)(procbrand+ 8) = cpuid_ex2.ecx;
@@ -299,13 +317,11 @@ public:
 		if (cpuid_0.eax >= 1)
 		{
 			cpuid(&cpuid_1, 1, 0);
-			_RPT4(_CRT_WARN, "CPUID.1   EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_1.eax, cpuid_1.ebx, cpuid_1.ecx, cpuid_1.edx);
 		}
 
 		if (cpuid_0.eax >= 7)
 		{
 			cpuid(&cpuid_7_0, 7, 0);
-			_RPT4(_CRT_WARN, "CPUID.7.0 EAX=%08X EBX=%08X ECX=%08X EDX=%08X\n", cpuid_7_0.eax, cpuid_7_0.ebx, cpuid_7_0.ecx, cpuid_7_0.edx);
 		}
 
 		if (cpuid_7_0.ebx & (1 << 3))
@@ -330,7 +346,6 @@ public:
 			xgetbv_result xgetbv_0 = { 0, 0 };
 
 			xgetbv(&xgetbv_0, 0);
-			_RPT2(_CRT_WARN, "XGETBV.0  EAX=%08X EDX=%08X\n", xgetbv_0.eax, xgetbv_0.edx);
 
 			if ((xgetbv_0.eax & 6) == 6)
 			{
