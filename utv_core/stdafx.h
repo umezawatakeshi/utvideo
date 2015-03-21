457c8ce9d1e3b0aab5f993b24e6b2dbd9a283fa2
UMEZAWA Takeshi <umezawa@utsoft.org>
2015-03-21 08:28:33 +0000

    printf/scanf において size_t のためのサイズ指定子は VC++ と gcc で異なるので、 <inttypes.h> と同様なものをマクロ定義する。

--- utv_core/stdafx.h
+++ utv_core/stdafx.h
@@ -98,3 +98,21 @@ inline bool IS_ALIGNED(const void *p, uintptr_t a)
 }
 
 #include <LogWriter.h>
+
+#if defined(_MSC_VER)
+#define PRISZT_PREFIX "I"
+#elif defined(__GNUC__)
+#define PRISZT_PREFIX "z"
+#else
+#error
+#endif
+#define PRIuSZT PRISZT_PREFIX "u"
+#define PRIoSZT PRISZT_PREFIX "o"
+#define PRIxSZT PRISZT_PREFIX "x"
+#define PRIXSZT PRISZT_PREFIX "X"
+
+#if SIZE_MAX == 4294967295U
+#define PRIp "08" PRIXSZT
+#else
+#define PRIp "016" PRIXSZT
+#endif
