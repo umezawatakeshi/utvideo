86fc2be47c6f5c46355b6e928e9a4656feac9e2c
UMEZAWA Takeshi <umezawa@utsoft.org>
2015-03-28 14:02:54 +0000

    DEBUG_ENTER_LEAVE はもう使われていない。

--- utv_core/utvideo.h
+++ utv_core/utvideo.h
@@ -125,32 +125,3 @@ typedef uint32_t utvf_t;
 #define UTVF_HDYC ((utvf_t)'HDYC')
 #define UTVF_YV12 ((utvf_t)'YV12')
 #define UTVF_v210 ((utvf_t)'v210')
-
-
-#ifdef _DEBUG
-class DebugEnterLeave
-{
-private:
-	char buf[256];
-public:
-	DebugEnterLeave(char *fmt, ...)
-	{
-		va_list argptr;
-		va_start(argptr, fmt);
-#pragma warning(push)
-#pragma warning(disable:4996)
-		vsnprintf(buf, _countof(buf), fmt, argptr);
-#pragma warning(pop)
-		va_end(argptr);
-		_RPT1(_CRT_WARN, "enter %s\n", buf);
-	}
-
-	~DebugEnterLeave()
-	{
-		_RPT1(_CRT_WARN, "leave %s\n", buf);
-	}
-};
-#define DEBUG_ENTER_LEAVE(...) class DebugEnterLeave debug_enter_leave_object(__VA_ARGS__)
-#else
-#define DEBUG_ENTER_LEAVE(...)
-#endif
