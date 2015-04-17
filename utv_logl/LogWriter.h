9714841efe67a841335d666371d8c2e61845c141
UMEZAWA Takeshi <umezawa@utsoft.org>
2015-04-17 14:55:19 +0000

    可変長引数マクロで、 "..." に相当する部分の引数がゼロ個でも 正しく動くのは Visual C++ 拡張であるらしい。

--- utv_logl/LogWriter.h
+++ utv_logl/LogWriter.h
@@ -36,12 +36,12 @@ static inline bool IsLogWriterInitializedOrDebugBuild()
 	return true;
 }
 
-#define LOGPRINTF(__fmt__, ...) \
+#define LOGPRINTF(...) \
 	do \
 	{ \
 		char __LOGPRINTF_local_buf1__[256]; \
 		char __LOGPRINTF_local_buf2__[256]; \
-		sprintf(__LOGPRINTF_local_buf1__, __fmt__, __VA_ARGS__); \
+		sprintf(__LOGPRINTF_local_buf1__, __VA_ARGS__); \
 		sprintf(__LOGPRINTF_local_buf2__, "<%s> %s", LOG_MODULE_NAME, __LOGPRINTF_local_buf1__); \
 		if (IsLogWriterInitialized()) \
 		{ \
@@ -51,12 +51,12 @@ static inline bool IsLogWriterInitializedOrDebugBuild()
 		OutputDebugStringA(__LOGPRINTF_local_buf2__); \
 	} while (false)
 
-#define DBGPRINTF(__fmt__, ...) \
+#define DBGPRINTF(...) \
 	do \
 	{ \
 		char __LOGPRINTF_local_buf1__[256]; \
 		char __LOGPRINTF_local_buf2__[256]; \
-		sprintf(__LOGPRINTF_local_buf1__, __fmt__, __VA_ARGS__); \
+		sprintf(__LOGPRINTF_local_buf1__, __VA_ARGS__); \
 		sprintf(__LOGPRINTF_local_buf2__, "<%s> %s", LOG_MODULE_NAME, __LOGPRINTF_local_buf1__); \
 		strcat(__LOGPRINTF_local_buf2__, "\n"); \
 		OutputDebugStringA(__LOGPRINTF_local_buf2__); \
@@ -69,20 +69,20 @@ static inline bool IsLogWriterInitializedOrDebugBuild()
 	return IsLogWriterInitialized();
 }
 
-#define LOGPRINTF(__fmt__, ...) \
+#define LOGPRINTF(...) \
 	do \
 	{ \
 		if (IsLogWriterInitialized()) \
 		{ \
 			char __LOGPRINTF_local_buf1__[256]; \
 			char __LOGPRINTF_local_buf2__[256]; \
-			sprintf(__LOGPRINTF_local_buf1__, __fmt__, __VA_ARGS__); \
+			sprintf(__LOGPRINTF_local_buf1__, __VA_ARGS__); \
 			sprintf(__LOGPRINTF_local_buf2__, "<%s> %s", LOG_MODULE_NAME, __LOGPRINTF_local_buf1__); \
 			WriteLog(__LOGPRINTF_local_buf2__); \
 		} \
 	} while (false)
 
-#define DBGPRINTF(__fmt__, ...) \
+#define DBGPRINTF(...) \
 	do \
 	{ \
 	} while (false)
