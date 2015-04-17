f53340cfe2e33f11eedf378c82d17f84f7157cc7
UMEZAWA Takeshi <umezawa@utsoft.org>
2015-04-17 17:04:01 +0000

    ログで引数を区切るカンマが抜けていた。

--- utv_vcm/utv_vcm.cpp
+++ utv_vcm/utv_vcm.cpp
@@ -15,11 +15,11 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
 	if (dwReason == DLL_PROCESS_ATTACH)
 	{
-		LOGPRINTF("DllMain(hModule=%" PRIp ", dwReason=DLL_PROCESS_ATTACH lpReserved=%" PRIp ")", hModule, lpReserved);
+		LOGPRINTF("DllMain(hModule=%" PRIp ", dwReason=DLL_PROCESS_ATTACH, lpReserved=%" PRIp ")", hModule, lpReserved);
 	}
 	else if (dwReason == DLL_PROCESS_DETACH)
 	{
-		LOGPRINTF("DllMain(hModule=%" PRIp ", dwReason=DLL_PROCESS_DETACH lpReserved=%" PRIp ")", hModule, lpReserved);
+		LOGPRINTF("DllMain(hModule=%" PRIp ", dwReason=DLL_PROCESS_DETACH, lpReserved=%" PRIp ")", hModule, lpReserved);
 	}
 
 	return TRUE;
