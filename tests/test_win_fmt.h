e77c43ed12a1a8b26cbc9072d70b732eb473b497
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-02-20 00:34:21 +0900

    Boost.Test フレームワークが FourCC である DWORD をログに出す場合に、出来る限り数値ではなく FourCC の見た目になるようにした。

--- tests/test_win_fmt.h
+++ tests/test_win_fmt.h
@@ -3,4 +3,29 @@
 
 #pragma once
 
+namespace boost { namespace test_tools { namespace tt_detail {
+
+template<>
+struct print_log_value<DWORD>
+{
+	void operator()(ostream& os, const DWORD& fcc)
+	{
+		union
+		{
+			DWORD dw;
+			char buf[16];
+		};
+		dw = fcc;
+
+		if (isprint(buf[0]) && isprint(buf[1]) && isprint(buf[2]) && isprint(buf[3]))
+			buf[4] = '\0';
+		else
+			sprintf(buf, "0x%08x", dw);
+
+		os << buf;
+	}
+};
+
+}}}
+
 extern vector<FOURCC> vecCodecFourcc;
