7c72d63478f26d054ce3fa8873f6bebd4a4958a0
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-02-24 02:55:09 +0900

    プラットフォームによってテストスイート名を変更する。

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,24 @@
 /* •¶šƒR[ƒh‚Í‚r‚i‚h‚r ‰üsƒR[ƒh‚Í‚b‚q‚k‚e */
 /* $Id$ */
 
-#define BOOST_TEST_MODULE tests
-#include <boost/test/unit_test.hpp> 
+#ifdef _M_X64
+#ifndef __x86_64__
+#define __x86_64__ 1
+#endif
+#endif
+
+#ifdef _M_IX86
+#ifndef __i386__
+#define __i386__ 1
+#endif
+#endif
+
+#ifdef __i386__
+#define BOOST_TEST_MODULE tests-x86
+#endif
+
+#ifdef __x86_64__
+#define BOOST_TEST_MODULE tests-x64
+#endif
+
+#include <boost/test/unit_test.hpp>
