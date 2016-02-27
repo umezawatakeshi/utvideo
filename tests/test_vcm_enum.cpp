8498be0226553b461809a56f44165ae40bc744c0
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-02-28 02:00:08 +0900

    VCM の ICInfo で FourCC を指定して情報を取った場合のテストを追加した。

--- tests/test_vcm_enum.cpp
+++ tests/test_vcm_enum.cpp
@@ -27,3 +27,21 @@ BOOST_DATA_TEST_CASE(vcm_ICInfo_enum, data::make(vecCodecFourcc), fcc)
 
 	BOOST_CHECK(found);
 }
+
+BOOST_DATA_TEST_CASE(vcm_ICInfo_get, data::make(vecCodecFourcc), fcc)
+{
+	ICINFO info;
+	BOOL b;
+
+	b = ICInfo(ICTYPE_VIDEO, fcc, &info);
+	BOOST_REQUIRE(b);
+
+	// info.fccHandler ‚Í¬•¶Žš‚Å•Ô‚Á‚Ä‚­‚é‚æ‚¤‚È‚Ì‚Å‘å•¶Žš‚É•ÏŠ·‚·‚é
+	DWORD fccUpper =
+		((toupper(info.fccHandler & 0xff) & 0xff)) |
+		((toupper((info.fccHandler >> 8) & 0xff) & 0xff) << 8) |
+		((toupper((info.fccHandler >> 16) & 0xff) & 0xff) << 16) |
+		((toupper((info.fccHandler >> 24) & 0xff) & 0xff) << 24);
+
+	BOOST_TEST_CHECK(fccUpper == fcc);
+}
