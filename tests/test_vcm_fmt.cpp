6cb088757f13f45a9166dc51912e18b11b406f44
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-02-28 01:35:44 +0900

    VCM の ICCompressGetFormat のテストで、実際のテスト関数をまとめた。

--- tests/test_vcm_fmt.cpp
+++ tests/test_vcm_fmt.cpp
@@ -7,12 +7,8 @@
 #define TEST_WIDTH 1920
 #define TEST_HEIGHT 1080
 
-BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
-BOOST_DATA_TEST_CASE(vcm_enc_fmt_ok, data::make(vecSupportedInputFormatPair), crf)
+void vcm_ICCompressGetFormat_ok(DWORD fccCodec, DWORD fccRaw, LONG width, LONG height)
 {
-	DWORD fccCodec = crf.fccCodec;
-	DWORD fccRaw = crf.fccRaw;
-
 	HIC hic;
 	LRESULT lr;
 	DWORD dw;
@@ -30,8 +26,8 @@ BOOST_DATA_TEST_CASE(vcm_enc_fmt_ok, data::make(vecSupportedInputFormatPair), cr
 
 	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
 	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = TEST_WIDTH;
-	bihIn.biHeight = TEST_HEIGHT * FCC2HeightSign(fccRaw);
+	bihIn.biWidth = width;
+	bihIn.biHeight = height * FCC2HeightSign(fccRaw);
 	bihIn.biPlanes = 1;
 	bihIn.biBitCount = FCC2BitCount(fccRaw);
 	bihIn.biCompression = FCC2Compression(fccRaw);
@@ -45,8 +41,8 @@ BOOST_DATA_TEST_CASE(vcm_enc_fmt_ok, data::make(vecSupportedInputFormatPair), cr
 	BOOST_REQUIRE(dw == ICERR_OK);
 
 	BOOST_TEST_CHECK(bihOut.biSize == gfret);
-	BOOST_TEST_CHECK(bihOut.biWidth == TEST_WIDTH);
-	BOOST_TEST_CHECK(bihOut.biHeight == TEST_HEIGHT);
+	BOOST_TEST_CHECK(bihOut.biWidth == width);
+	BOOST_TEST_CHECK(bihOut.biHeight == height);
 	BOOST_TEST_CHECK(bihOut.biPlanes == 1);
 //	BOOST_TEST_CHECK(bihOut.biBitCount == xxx); // XXX
 	BOOST_TEST_CHECK(bihOut.biCompression == fccCodec);
@@ -55,12 +51,8 @@ BOOST_DATA_TEST_CASE(vcm_enc_fmt_ok, data::make(vecSupportedInputFormatPair), cr
 	BOOST_CHECK(lr == ICERR_OK);
 }
 
-BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
-BOOST_DATA_TEST_CASE(vcm_enc_fmt_ng, data::make(vecUnsupportedInputFormatPair), crf)
+void vcm_ICCompressGetFormat_ng(DWORD fccCodec, DWORD fccRaw, LONG width, LONG height)
 {
-	DWORD fccCodec = crf.fccCodec;
-	DWORD fccRaw = crf.fccRaw;
-
 	HIC hic;
 	LRESULT lr;
 	DWORD dw;
@@ -78,8 +70,8 @@ BOOST_DATA_TEST_CASE(vcm_enc_fmt_ng, data::make(vecUnsupportedInputFormatPair), 
 
 	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
 	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = TEST_WIDTH;
-	bihIn.biHeight = TEST_HEIGHT * FCC2HeightSign(fccRaw);
+	bihIn.biWidth = width;
+	bihIn.biHeight = height * FCC2HeightSign(fccRaw);
 	bihIn.biPlanes = 1;
 	bihIn.biBitCount = FCC2BitCount(fccRaw);
 	bihIn.biCompression = FCC2Compression(fccRaw);
@@ -102,183 +94,37 @@ BOOST_DATA_TEST_CASE(vcm_enc_fmt_ng, data::make(vecUnsupportedInputFormatPair), 
 }
 
 BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
+BOOST_DATA_TEST_CASE(vcm_enc_fmt_ok, data::make(vecSupportedInputFormatPair), crf)
+{
+	vcm_ICCompressGetFormat_ok(crf.fccCodec, crf.fccRaw, TEST_WIDTH, TEST_HEIGHT);
+}
+
+BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
+BOOST_DATA_TEST_CASE(vcm_enc_fmt_ng, data::make(vecUnsupportedInputFormatPair), crf)
+{
+	vcm_ICCompressGetFormat_ng(crf.fccCodec, crf.fccRaw, TEST_WIDTH, TEST_HEIGHT);
+}
+
+BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
 BOOST_DATA_TEST_CASE(vcm_enc_fmt_width_ok, data::make(vecSupportedWidthPair), swp)
 {
-	DWORD fccCodec = swp.first.first;
-	DWORD fccRaw = swp.first.second;
-	LONG width = swp.second;
-
-	HIC hic;
-	LRESULT lr;
-	DWORD dw;
-	DWORD gfret;
-
-	BITMAPINFOHEADER bihIn;
-	union
-	{
-		BITMAPINFOHEADER bihOut;
-		char bihOutBuf[128];
-	};
-
-	hic = ICOpen(ICTYPE_VIDEO, fccCodec, ICMODE_COMPRESS);
-	BOOST_REQUIRE(hic != NULL);
-
-	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
-	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = width;
-	bihIn.biHeight = TEST_HEIGHT * FCC2HeightSign(fccRaw);
-	bihIn.biPlanes = 1;
-	bihIn.biBitCount = FCC2BitCount(fccRaw);
-	bihIn.biCompression = FCC2Compression(fccRaw);
-	bihIn.biSizeImage = 10000000;
-
-	gfret = ICCompressGetFormatSize(hic, &bihIn);
-	BOOST_TEST_REQUIRE(gfret >= sizeof(BITMAPINFOHEADER));
-	BOOST_TEST_REQUIRE(gfret <= sizeof(bihOutBuf));
-
-	dw = ICCompressGetFormat(hic, &bihIn, &bihOut);
-	BOOST_REQUIRE(dw == ICERR_OK);
-
-	BOOST_TEST_CHECK(bihOut.biSize == gfret);
-	BOOST_TEST_CHECK(bihOut.biWidth == width);
-	BOOST_TEST_CHECK(bihOut.biHeight == TEST_HEIGHT);
-	BOOST_TEST_CHECK(bihOut.biPlanes == 1);
-	//	BOOST_TEST_CHECK(bihOut.biBitCount == xxx); // XXX
-	BOOST_TEST_CHECK(bihOut.biCompression == fccCodec);
-
-	lr = ICClose(hic);
-	BOOST_CHECK(lr == ICERR_OK);
+	vcm_ICCompressGetFormat_ok(swp.first.first, swp.first.second, swp.second, TEST_HEIGHT);
 }
 
 BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
 BOOST_DATA_TEST_CASE(vcm_enc_fmt_width_ng, data::make(vecUnsupportedWidthPair), swp)
 {
-	DWORD fccCodec = swp.first.first;
-	DWORD fccRaw = swp.first.second;
-	LONG width = swp.second;
-
-	HIC hic;
-	LRESULT lr;
-	DWORD dw;
-	DWORD gfret;
-
-	BITMAPINFOHEADER bihIn;
-	union
-	{
-		BITMAPINFOHEADER bihOut;
-		char bihOutBuf[128];
-	};
-
-	hic = ICOpen(ICTYPE_VIDEO, fccCodec, ICMODE_COMPRESS);
-	BOOST_REQUIRE(hic != NULL);
-
-	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
-	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = width;
-	bihIn.biHeight = TEST_HEIGHT * FCC2HeightSign(fccRaw);
-	bihIn.biPlanes = 1;
-	bihIn.biBitCount = FCC2BitCount(fccRaw);
-	bihIn.biCompression = FCC2Compression(fccRaw);
-	bihIn.biSizeImage = 10000000;
-
-	gfret = ICCompressGetFormatSize(hic, &bihIn);
-	BOOST_TEST_REQUIRE(gfret >= sizeof(BITMAPINFOHEADER));
-	BOOST_TEST_REQUIRE(gfret <= sizeof(bihOutBuf));
-
-	dw = ICCompressGetFormat(hic, &bihIn, &bihOut);
-	BOOST_CHECK(dw != ICERR_OK);
-
-	lr = ICClose(hic);
-	BOOST_CHECK(lr == ICERR_OK);
+	vcm_ICCompressGetFormat_ng(swp.first.first, swp.first.second, swp.second, TEST_HEIGHT);
 }
 
 BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
 BOOST_DATA_TEST_CASE(vcm_enc_fmt_height_ok, data::make(vecSupportedHeightPair), swp)
 {
-	DWORD fccCodec = swp.first.first;
-	DWORD fccRaw = swp.first.second;
-	LONG height = swp.second;
-
-	HIC hic;
-	LRESULT lr;
-	DWORD dw;
-	DWORD gfret;
-
-	BITMAPINFOHEADER bihIn;
-	union
-	{
-		BITMAPINFOHEADER bihOut;
-		char bihOutBuf[128];
-	};
-
-	hic = ICOpen(ICTYPE_VIDEO, fccCodec, ICMODE_COMPRESS);
-	BOOST_REQUIRE(hic != NULL);
-
-	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
-	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = TEST_WIDTH;
-	bihIn.biHeight = height * FCC2HeightSign(fccRaw);
-	bihIn.biPlanes = 1;
-	bihIn.biBitCount = FCC2BitCount(fccRaw);
-	bihIn.biCompression = FCC2Compression(fccRaw);
-	bihIn.biSizeImage = 10000000;
-
-	gfret = ICCompressGetFormatSize(hic, &bihIn);
-	BOOST_TEST_REQUIRE(gfret >= sizeof(BITMAPINFOHEADER));
-	BOOST_TEST_REQUIRE(gfret <= sizeof(bihOutBuf));
-
-	dw = ICCompressGetFormat(hic, &bihIn, &bihOut);
-	BOOST_REQUIRE(dw == ICERR_OK);
-
-	BOOST_TEST_CHECK(bihOut.biSize == gfret);
-	BOOST_TEST_CHECK(bihOut.biWidth == TEST_WIDTH);
-	BOOST_TEST_CHECK(bihOut.biHeight == height);
-	BOOST_TEST_CHECK(bihOut.biPlanes == 1);
-	//	BOOST_TEST_CHECK(bihOut.biBitCount == xxx); // XXX
-	BOOST_TEST_CHECK(bihOut.biCompression == fccCodec);
-
-	lr = ICClose(hic);
-	BOOST_CHECK(lr == ICERR_OK);
+	vcm_ICCompressGetFormat_ok(swp.first.first, swp.first.second, TEST_WIDTH, swp.second);
 }
 
 BOOST_TEST_DECORATOR(*depends_on("vcm_open_enc"))
 BOOST_DATA_TEST_CASE(vcm_enc_fmt_height_ng, data::make(vecUnsupportedHeightPair), swp)
 {
-	DWORD fccCodec = swp.first.first;
-	DWORD fccRaw = swp.first.second;
-	LONG height = swp.second;
-
-	HIC hic;
-	LRESULT lr;
-	DWORD dw;
-	DWORD gfret;
-
-	BITMAPINFOHEADER bihIn;
-	union
-	{
-		BITMAPINFOHEADER bihOut;
-		char bihOutBuf[128];
-	};
-
-	hic = ICOpen(ICTYPE_VIDEO, fccCodec, ICMODE_COMPRESS);
-	BOOST_REQUIRE(hic != NULL);
-
-	memset(&bihIn, 0, sizeof(BITMAPINFOHEADER));
-	bihIn.biSize = sizeof(BITMAPINFOHEADER);
-	bihIn.biWidth = TEST_WIDTH;
-	bihIn.biHeight = height * FCC2HeightSign(fccRaw);
-	bihIn.biPlanes = 1;
-	bihIn.biBitCount = FCC2BitCount(fccRaw);
-	bihIn.biCompression = FCC2Compression(fccRaw);
-	bihIn.biSizeImage = 10000000;
-
-	gfret = ICCompressGetFormatSize(hic, &bihIn);
-	BOOST_TEST_REQUIRE(gfret >= sizeof(BITMAPINFOHEADER));
-	BOOST_TEST_REQUIRE(gfret <= sizeof(bihOutBuf));
-
-	dw = ICCompressGetFormat(hic, &bihIn, &bihOut);
-	BOOST_CHECK(dw != ICERR_OK);
-
-	lr = ICClose(hic);
-	BOOST_CHECK(lr == ICERR_OK);
+	vcm_ICCompressGetFormat_ng(swp.first.first, swp.first.second, TEST_WIDTH, swp.second);
 }
