4e641d57560e9e70277f2b6142d0445943c52c2f
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-03-26 02:35:59 +0900

    DMO での DMOGetName のテストを追加した。

---
+++ tests/test_dmo_DMOGetName.cpp
@@ -0,0 +1,28 @@
+/* ï∂éöÉRÅ[ÉhÇÕÇrÇiÇhÇr â¸çsÉRÅ[ÉhÇÕÇbÇqÇkÇe */
+/* $Id$ */
+
+#include "stdafx.h"
+#include "test_win_fmt.h"
+#include "test_dmo_fmt.h"
+
+void dmo_DMOGetName(REFGUID clsidCodec, wstring wstrName)
+{
+	HRESULT hr;
+	WCHAR name[128];
+
+	hr = DMOGetName(clsidCodec, name);
+	BOOST_REQUIRE(hr == S_OK);
+	BOOST_CHECK_EQUAL(wstring(name), wstrName);
+}
+
+BOOST_DATA_TEST_CASE(dmo_DMOGetName_encoder, data::make(vecCodecFourcc) ^ data::make(vecEncoderName), fcc, wstrName)
+{
+	DMOEncoderCLSID guid(fcc);
+	dmo_DMOGetName(guid, wstrName);
+}
+
+BOOST_DATA_TEST_CASE(dmo_DMOGetName_decoder, data::make(vecCodecFourcc) ^ data::make(vecDecoderName), fcc, wstrName)
+{
+	DMODecoderCLSID guid(fcc);
+	dmo_DMOGetName(guid, wstrName);
+}
