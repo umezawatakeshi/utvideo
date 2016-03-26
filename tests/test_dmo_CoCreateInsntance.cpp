c3535c4849bb901efb4ad2da55801fe16bf5b247
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-03-26 23:46:53 +0900

    DMO での CoCreateInstance のテストを追加した。

---
+++ tests/test_dmo_CoCreateInsntance.cpp
@@ -0,0 +1,30 @@
+/* •¶ŽšƒR[ƒh‚Í‚r‚i‚h‚r ‰üsƒR[ƒh‚Í‚b‚q‚k‚e */
+/* $Id$ */
+
+#include "stdafx.h"
+#include "test_win_fmt.h"
+#include "test_dmo_fmt.h"
+
+void dmo_CoCreateInstance(REFGUID clsidCodec)
+{
+	HRESULT hr;
+	IUnknown *pUnk;
+
+	hr = CoCreateInstance(clsidCodec, NULL, CLSCTX_INPROC_SERVER, IID_IUnknown, (LPVOID*)&pUnk);
+	BOOST_REQUIRE_EQUAL(hr, S_OK);
+	BOOST_REQUIRE(pUnk != NULL);
+
+	pUnk->Release();
+}
+
+BOOST_DATA_TEST_CASE(dmo_CoCreateInstance_encoder, data::make(vecCodecFourcc), fcc)
+{
+	DMOEncoderCLSID guid(fcc);
+	dmo_CoCreateInstance(guid);
+}
+
+BOOST_DATA_TEST_CASE(dmo_CoCreateInstance_decoder, data::make(vecCodecFourcc), fcc)
+{
+	DMODecoderCLSID guid(fcc);
+	dmo_CoCreateInstance(guid);
+}
