d278022529f42d4b9945372620359113183803cb
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-07-20 02:47:15 +0900

    ビルドしてテストするバッチファイルを追加した。

---
+++ build_and_test_win.bat
@@ -0,0 +1,31 @@
+setlocal
+del tests-*.xml
+set MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+%MSBUILD% utvideo.sln /p:Configuration=Release /p:Platform=Win32 /t:Rebuild
+if ERRORLEVEL 1 goto errexit
+
+%MSBUILD% utvideo.sln /p:Configuration=Release /p:Platform=x64 /t:Rebuild
+if ERRORLEVEL 1 goto errexit
+
+"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" setup_win\setup_win.iss
+if ERRORLEVEL 1 goto errexit
+
+set ORIGPATH=%PATH%
+set ERRLVL=0
+
+set PATH=%FFMPEG_LIB_X86%;%ORIGPATH%
+Release\tests.exe --log_format=XML --log_sink=tests-x86.xml --log_level=all
+if ERRORLEVEL 1 set ERRLVL=%ERRORLEVEL%
+
+set PATH=%FFMPEG_LIB_X64%;%ORIGPATH%
+x64\Release\tests.exe --log_format=XML --log_sink=tests-x64.xml --log_level=all
+if ERRORLEVEL 1 set ERRLVL=%ERRORLEVEL%
+
+set PATH=%ORIGPATH%
+if "%ERRLVL%" NEQ "0" goto errexit
+
+exit /b 0
+
+:errexit
+exit /b 1
