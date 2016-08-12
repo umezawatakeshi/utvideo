9054fcc9912131b768e863941369ea38ca5a706a
UMEZAWA Takeshi <umezawa@utsoft.org>
2016-08-12 18:50:42 +0900

    グレークリップテストのテストケースを拡充した。

--- tests/test_clip.cpp
+++ tests/test_clip.cpp
@@ -8,24 +8,64 @@
 
 #include "test_clip.h"
 
-static vector<pair<string, vector<pair<string, unsigned int>>>> vecDecodeClipsSource = {
-	{ "clip000-ulrg-progressive-left-div8-384x256.avi",{ { "clip000-raw-rgb24-384x256.avi", 0 },{ "clip000-raw-rgb32-384x256.avi", 0 }, } },
-	{ "clip000-ulra-progressive-left-div8-384x256.avi",{ { "clip000-raw-rgba-384x256.avi", 0 }, } },
-	{ "clip000-uly2-progressive-left-div8-384x256.avi",{ { "clip000-raw-yuy2-384x256.avi", 0 },{ "clip000-raw-uyvy-384x256.avi", 0 }, } },
-	{ "clip000-uly0-progressive-left-div8-384x256.avi",{ { "clip000-raw-yv12-384x256.avi", 0 },{ "clip000-raw-yuy2-384x256.avi", 0 },{ "clip000-raw-uyvy-384x256.avi", 0 }, } },
-	{ "clip000-ulh2-progressive-left-div8-384x256.avi",{ { "clip000-raw-yuy2-384x256.avi", 0 },{ "clip000-raw-uyvy-384x256.avi", 0 }, } },
-	{ "clip000-ulh0-progressive-left-div8-384x256.avi",{ { "clip000-raw-yv12-384x256.avi", 0 },{ "clip000-raw-yuy2-384x256.avi", 0 },{ "clip000-raw-uyvy-384x256.avi", 0 }, } },
+static vector<tuple<string, int, int, vector<pair<string, unsigned int>>>> vecDecodeClipsSource = {
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulrg-progressive", 1, 1, { { "clip000-raw-rgb24", 0 },{ "clip000-raw-rgb32", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulra-progressive", 1, 1, { { "clip000-raw-rgba", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-uly2-progressive", 2, 1, { { "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-uly0-progressive", 2, 2, { { "clip000-raw-yv12", 0 },{ "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulh2-progressive", 2, 1, { { "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulh0-progressive", 2, 2, { { "clip000-raw-yv12", 0 },{ "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, } ),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulrg-interlace", 1, 2,{ { "clip000-raw-rgb24", 0 },{ "clip000-raw-rgb32", 0 }, }),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulra-interlace", 1, 2,{ { "clip000-raw-rgba", 0 }, }),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-uly2-interlace", 2, 2,{ { "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, }),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-uly0-interlace", 2, 4,{ { "clip000-raw-yv12", 0 },{ "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, }),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulh2-interlace", 2, 2,{ { "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, }),
+	make_tuple<string, int, int, vector<pair<string, unsigned int>>>("clip000-ulh0-interlace", 2, 4,{ { "clip000-raw-yv12", 0 },{ "clip000-raw-yuy2", 0 },{ "clip000-raw-uyvy", 0 }, }),
 };
 
-vector<tuple<string, string, unsigned int>> conv(const vector<pair<string, vector<pair<string, unsigned int>>>> &v)
+static int divs[] = { 1, 8, 11 };
+static string preds[] = { "left", "median" };
+
+vector<tuple<string, string, unsigned int>> conv(const vector<tuple<string, int, int, vector<pair<string, unsigned int>>>> &v)
 {
 	vector<tuple<string, string, unsigned int>> r;
 
-	for (auto i : v)
+	for (auto& i : v)
 	{
-		for (auto j : i.second)
+		auto& compbase = std::get<0>(i);
+		auto widthstep = std::get<1>(i);
+		auto heightstep = std::get<2>(i);
+		for (auto& j : std::get<3>(i))
 		{
-			r.push_back(make_tuple(i.first, j.first, j.second));
+			auto& rawbase = j.first;
+			auto& tolerance = j.second;
+			for (auto div : divs)
+			{
+				for (auto& pred : preds)
+				{
+					char comp[256], raw[256];
+
+					sprintf(comp, "%s-%s-div%d-384x512.avi", compbase.c_str(), pred.c_str(), div);
+					sprintf(raw, "%s-384x512.avi", rawbase.c_str());
+					r.push_back(make_tuple(comp, raw, tolerance));
+
+					for (int width = 384; width > 380; width -= widthstep)
+					{
+						sprintf(comp, "%s-%s-div%d-%dx256.avi", compbase.c_str(), pred.c_str(), div, width);
+						sprintf(raw, "%s-%dx256.avi", rawbase.c_str(), width);
+						r.push_back(make_tuple(comp, raw, tolerance));
+					}
+
+					for (int height = 256; height > 252; height -= heightstep)
+					{
+						if (height == 256)
+							continue;
+						sprintf(comp, "%s-%s-div%d-384x%d.avi", compbase.c_str(), pred.c_str(), div, height);
+						sprintf(raw, "%s-384x%d.avi", rawbase.c_str(), height);
+						r.push_back(make_tuple(comp, raw, tolerance));
+					}
+				}
+			}
 		}
 	}
 
