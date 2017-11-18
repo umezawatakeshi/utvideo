93c631da1bcbe8a018de019885dfdd81184648ed
UMEZAWA Takeshi <umezawa@utsoft.org>
2017-11-19 02:59:46 +0900

    エンコードで2ブロック同時に処理することで高速化した。

--- utv_core/UTRDCodec.cpp
+++ utv_core/UTRDCodec.cpp
@@ -112,47 +112,60 @@ size_t CUTRDCodec::EncodeFrame(void *pOutput, bool *pbKeyFrame, const void *pInp
 		auto pCompressedBegin = p;
 		for (unsigned y = 0; y < m_nHeight; y += 1)
 		{
-			for (unsigned x = 0; x < m_nWidth; x += 8)
+			for (unsigned x = 0; x < m_nWidth; x += 16)
 			{
-				uint64_t wi = *(uint64_t*)&m_pMedianPredicted->GetPlane(nPlaneIndex)[y * m_nWidth + x];
-				__m128i w = _mm_cvtsi64x_si128(wi);
+				__m128i w = _mm_loadu_si128((const __m128i *)&m_pMedianPredicted->GetPlane(nPlaneIndex)[y * m_nWidth + x]);
+				__m128i visnotzero = _mm_cmpeq_epi64(_mm_cmpeq_epi64(w, _mm_setzero_si128()), _mm_setzero_si128());
 
-				int mode;
-				int bits;
+				int mode0, mode1;
+				int bits0, bits1;
 
-				if (wi == 0)
-				{
-					mode = 0;
-					bits = 0;
-				}
-				else
 				{
 					__m128i z;
 					__m128i signs = _mm_cmpgt_epi8(_mm_setzero_si128(), w);
+					int isnotzero0 = _mm_cvtsi128_si32(visnotzero);
+					int isnotzero1 = _mm_extract_epi32(visnotzero, 2);
 					z = _mm_xor_si128(w, signs);
 					z = _mm_or_si128(z, _mm_srli_epi64(z, 32));
 					z = _mm_or_si128(z, _mm_srli_epi64(z, 16));
 					z = _mm_or_si128(z, _mm_srli_epi64(z, 8));
 					z = _mm_and_si128(z, _mm_set1_epi64x(0xff));
 					z = _mm_or_si128(z, _mm_set1_epi64x(1));
-					_BitScanReverse((unsigned long *)&mode, _mm_cvtsi128_si32(z));
-					bits = mode + 2;
-					int rembits = 6 - mode;
-					mode++;
-					__m128i vrembits = _mm_cvtsi64x_si128(rembits);
-					w = _mm_add_epi8(w, _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits));
-					w = _mm_or_si128(_mm_and_si128(w, _mm_set1_epi16(0x00ff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi16(0x00ff), w), vrembits));
-					vrembits = _mm_slli_epi64(vrembits, 1);
-					w = _mm_or_si128(_mm_and_si128(w, _mm_set1_epi32(0x0000ffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi32(0x0000ffff), w), vrembits));
-					vrembits = _mm_slli_epi64(vrembits, 1);
-					w = _mm_or_si128(_mm_and_si128(w, _mm_set1_epi64x(0x00000000ffffffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi64x(0x00000000ffffffff), w), vrembits));
-					*(uint64_t*)p = _mm_cvtsi128_si64x(w);
+					_BitScanReverse((unsigned long *)&mode0, _mm_cvtsi128_si32(z));
+					_BitScanReverse((unsigned long *)&mode1, _mm_extract_epi32(z, 2));
+					bits0 = mode0 + 2;
+					int rembits0 = 6 - mode0;
+					mode0++;
+					bits1 = mode1 + 2;
+					int rembits1 = 6 - mode1;
+					mode1++;
+
+					bits0 &= isnotzero0;
+					mode0 &= isnotzero0;
+					bits1 &= isnotzero1;
+					mode1 &= isnotzero1;
+					__m128i vrembits0 = _mm_cvtsi64x_si128(rembits0);
+					__m128i vrembits1 = _mm_cvtsi64x_si128(rembits1);
+					__m128i w0 = _mm_add_epi8(w, _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits0));
+					__m128i w1 = _mm_add_epi8(_mm_srli_si128(w, 8), _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits1));
+					w0 = _mm_or_si128(_mm_and_si128(w0, _mm_set1_epi16(0x00ff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi16(0x00ff), w0), vrembits0));
+					w1 = _mm_or_si128(_mm_and_si128(w1, _mm_set1_epi16(0x00ff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi16(0x00ff), w1), vrembits1));
+					vrembits0 = _mm_slli_epi64(vrembits0, 1);
+					vrembits1 = _mm_slli_epi64(vrembits1, 1);
+					w0 = _mm_or_si128(_mm_and_si128(w0, _mm_set1_epi32(0x0000ffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi32(0x0000ffff), w0), vrembits0));
+					w1 = _mm_or_si128(_mm_and_si128(w1, _mm_set1_epi32(0x0000ffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi32(0x0000ffff), w1), vrembits1));
+					vrembits0 = _mm_slli_epi64(vrembits0, 1);
+					vrembits1 = _mm_slli_epi64(vrembits1, 1);
+					w0 = _mm_or_si128(_mm_and_si128(w0, _mm_set1_epi64x(0x00000000ffffffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi64x(0x00000000ffffffff), w0), vrembits0));
+					w1 = _mm_or_si128(_mm_and_si128(w1, _mm_set1_epi64x(0x00000000ffffffff)), _mm_srl_epi64(_mm_andnot_si128(_mm_set1_epi64x(0x00000000ffffffff), w1), vrembits1));
+					*(uint64_t*)p = _mm_cvtsi128_si64x(w0);
+					*(uint64_t*)(p+bits0) = _mm_cvtsi128_si64x(w1);
 				}
 
-				p += bits;
-				*(uint32_t *)idxp |= (mode << shift);
+				p += bits0 + bits1;
 #define MODEBITS 3
-				shift += MODEBITS;
+				*(uint32_t *)idxp |= (((mode1 << MODEBITS) | mode0) << shift);
+				shift += MODEBITS*2;
 				if (shift == MODEBITS*8)
 				{
 					idxp += MODEBITS;
