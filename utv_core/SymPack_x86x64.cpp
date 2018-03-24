e92da302c6a9bc8987d726467a5a473c30fea1b3
UMEZAWA Takeshi <umezawa@utsoft.org>
2018-03-24 20:08:44 +0900

    ベクトル化部分を増やして高速化した。

--- utv_core/SymPack_x86x64.cpp
+++ utv_core/SymPack_x86x64.cpp
@@ -10,69 +10,45 @@
 template<int F>
 static inline FORCEINLINE void packer(uint8_t*& q, uint8_t*& r, int& shift, __m128i wa, __m128i wb)
 {
-	__m128i visnotzeroa = _mm_cmpeq_epi64(_mm_cmpeq_epi64(wa, _mm_setzero_si128()), _mm_setzero_si128());
-	__m128i visnotzerob = _mm_cmpeq_epi64(_mm_cmpeq_epi64(wb, _mm_setzero_si128()), _mm_setzero_si128());
+	__m128i viszeroa = _mm_cmpeq_epi64(wa, _mm_setzero_si128());
+	__m128i viszerob = _mm_cmpeq_epi64(wb, _mm_setzero_si128());
+	__m128i viszero = _mm_blend_epi16(viszeroa, viszerob, 0xcc);
 
-	int mode0, mode1, mode2, mode3;
 	int bits0, bits1, bits2, bits3;
 
 	__m128i za, zb;
 	__m128i signsa = _mm_cmpgt_epi8(_mm_setzero_si128(), wa);
 	__m128i signsb = _mm_cmpgt_epi8(_mm_setzero_si128(), wb);
-	int isnotzero0 = _mm_cvtsi128_si32(visnotzeroa);
-	int isnotzero1 = _mm_extract_epi32(visnotzeroa, 2);
-	int isnotzero2 = _mm_cvtsi128_si32(visnotzerob);
-	int isnotzero3 = _mm_extract_epi32(visnotzerob, 2);
 	za = _mm_xor_si128(wa, signsa);
 	zb = _mm_xor_si128(wb, signsb);
 	za = _mm_or_si128(za, _mm_srli_epi64(za, 32));
-	zb = _mm_or_si128(zb, _mm_srli_epi64(zb, 32));
-	za = _mm_or_si128(za, _mm_srli_epi64(za, 16));
-	zb = _mm_or_si128(zb, _mm_srli_epi64(zb, 16));
-	za = _mm_or_si128(za, _mm_srli_epi64(za, 8));
-	zb = _mm_or_si128(zb, _mm_srli_epi64(zb, 8));
-	za = _mm_and_si128(za, _mm_set1_epi64x(0xff));
-	zb = _mm_and_si128(zb, _mm_set1_epi64x(0xff));
-	za = _mm_or_si128(za, _mm_set1_epi64x(1));
-	zb = _mm_or_si128(zb, _mm_set1_epi64x(1));
-#if defined(_MSC_VER)
-	_BitScanReverse((unsigned long *)&mode0, _mm_cvtsi128_si32(za));
-	_BitScanReverse((unsigned long *)&mode1, _mm_extract_epi32(za, 2));
-	_BitScanReverse((unsigned long *)&mode2, _mm_cvtsi128_si32(zb));
-	_BitScanReverse((unsigned long *)&mode3, _mm_extract_epi32(zb, 2));
-#elif defined(__GNUC__)
-	mode0 = 31 - __builtin_clz(_mm_cvtsi128_si32(za));
-	mode1 = 31 - __builtin_clz(_mm_extract_epi32(za, 2));
-	mode2 = 31 - __builtin_clz(_mm_cvtsi128_si32(zb));
-	mode3 = 31 - __builtin_clz(_mm_extract_epi32(zb, 2));
-#else
-#error
-#endif
-	bits0 = mode0 + 2;
-	int rembits0 = 6 - mode0;
-	mode0++;
-	bits1 = mode1 + 2;
-	int rembits1 = 6 - mode1;
-	mode1++;
-	bits2 = mode2 + 2;
-	int rembits2 = 6 - mode2;
-	mode2++;
-	bits3 = mode3 + 2;
-	int rembits3 = 6 - mode3;
-	mode3++;
+	zb = _mm_or_si128(zb, _mm_slli_epi64(zb, 32));
+	__m128i z = _mm_blend_epi16(za, zb, 0xcc);
+	z = _mm_or_si128(z, _mm_srli_epi32(z, 16));
+	z = _mm_or_si128(z, _mm_srli_epi32(z, 8));
+	z = _mm_and_si128(z, _mm_set1_epi32(0xff));
+	z = _mm_or_si128(z, _mm_set1_epi32(1));
 
-	bits0 &= isnotzero0;
-	mode0 &= isnotzero0;
-	bits1 &= isnotzero1;
-	mode1 &= isnotzero1;
-	bits2 &= isnotzero2;
-	mode2 &= isnotzero2;
-	bits3 &= isnotzero3;
-	mode3 &= isnotzero3;
-	__m128i vrembits0 = _mm_cvtsi32_si128(rembits0);
-	__m128i vrembits1 = _mm_cvtsi32_si128(rembits1);
-	__m128i vrembits2 = _mm_cvtsi32_si128(rembits2);
-	__m128i vrembits3 = _mm_cvtsi32_si128(rembits3);
+
+	z = _mm_castps_si128(_mm_cvtepi32_ps(z));
+	z = _mm_srli_epi32(z, 23);
+	z = _mm_sub_epi32(z, _mm_set1_epi32(0x7d));
+	__m128i vbits = z;
+	__m128i vrembits = _mm_sub_epi32(_mm_set1_epi32(8), vbits);
+	vbits = _mm_andnot_si128(viszero, vbits);
+	__m128i vmode = _mm_add_epi32(vbits, _mm_cmpgt_epi32(vbits, _mm_setzero_si128()));
+
+	bits0 = _mm_cvtsi128_si32(vbits);
+	bits1 = _mm_extract_epi32(vbits, 2);
+	bits2 = _mm_extract_epi32(vbits, 1);
+	bits3 = _mm_extract_epi32(vbits, 3);
+
+	__m128i vrembitsa = _mm_and_si128(vrembits, _mm_set1_epi64x(0xffffffff));
+	__m128i vrembits0 = vrembitsa;
+	__m128i vrembits1 = _mm_srli_si128(vrembitsa, 8);
+	__m128i vrembitsb = _mm_srli_epi64(vrembits, 32);
+	__m128i vrembits2 = vrembitsb;
+	__m128i vrembits3 = _mm_srli_si128(vrembitsb, 8);
 	__m128i w0 = _mm_add_epi8(wa, _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits0));
 	__m128i w1 = _mm_add_epi8(_mm_srli_si128(wa, 8), _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits1));
 	__m128i w2 = _mm_add_epi8(wb, _mm_srl_epi64(_mm_set1_epi8((char)0x80), vrembits2));
@@ -106,7 +82,17 @@ static inline FORCEINLINE void packer(uint8_t*& q, uint8_t*& r, int& shift, __m1
 	_mm_storel_epi64((__m128i*)q, w3);
 	q += bits3;
 
-	*(uint32_t *)r |= (((mode3 << 9) | (mode2 << 6) | (mode1 << 3) | mode0) << shift);
+	vmode = _mm_shuffle_epi8(vmode, _mm_set_epi8(-1, -1, -1, -1, -1, -1, -1, -1, 13, 12, 9, 8, 5, 4, 1, 0));
+#ifdef __x86_64__
+	uint64_t modes = _mm_cvtsi128_si64(vmode);
+	modes |= modes >> (32 - 3);
+#else
+	vmode = _mm_or_si128(vmode, _mm_srli_epi64(vmode, 32 - 3));
+	uint32_t modes = _mm_cvtsi128_si32(vmode);
+#endif
+	modes |= modes >> (16 - 6);
+
+	*(uint32_t *)r |= (((uint32_t)(uint16_t)modes) << shift);
 	shift += 12;
 	if (shift == 24)
 	{
@@ -179,8 +165,8 @@ template void tuned_Pack8SymAfterPredictPlanarGradient8<CODEFEATURE_AVX1>(uint8_
 template<int F>
 static inline FORCEINLINE void packer(uint8_t*& q, uint8_t*& r, __m256i wa, __m256i wb)
 {
-	__m256i visnotzeroa = _mm256_cmpeq_epi64(_mm256_cmpeq_epi64(wa, _mm256_setzero_si256()), _mm256_setzero_si256());
-	__m256i visnotzerob = _mm256_cmpeq_epi64(_mm256_cmpeq_epi64(wb, _mm256_setzero_si256()), _mm256_setzero_si256());
+	__m256i viszeroa = _mm256_cmpeq_epi64(wa, _mm256_setzero_si256());
+	__m256i viszerob = _mm256_cmpeq_epi64(wb, _mm256_setzero_si256());
 
 	int modesa, modesb;
 	int bits0, bits1, bits2, bits3, bits4, bits5, bits6, bits7;
@@ -213,12 +199,10 @@ static inline FORCEINLINE void packer(uint8_t*& q, uint8_t*& r, __m256i wa, __m2
 	__m256i vbitsb = zb;
 	__m256i vrembitsa = _mm256_sub_epi64(_mm256_set1_epi64x(8), vbitsa);
 	__m256i vrembitsb = _mm256_sub_epi64(_mm256_set1_epi64x(8), vbitsb);
-	__m256i vmodea = _mm256_sub_epi64(vbitsa, _mm256_set1_epi64x(1));
-	__m256i vmodeb = _mm256_sub_epi64(vbitsb, _mm256_set1_epi64x(1));
-	vbitsa = _mm256_and_si256(vbitsa, visnotzeroa);
-	vbitsb = _mm256_and_si256(vbitsb, visnotzerob);
-	vmodea = _mm256_and_si256(vmodea, visnotzeroa);
-	vmodeb = _mm256_and_si256(vmodeb, visnotzerob);
+	vbitsa = _mm256_andnot_si256(viszeroa, vbitsa);
+	vbitsb = _mm256_andnot_si256(viszerob, vbitsb);
+	__m256i vmodea = _mm256_add_epi64(vbitsa, _mm256_cmpgt_epi64(vbitsa, _mm256_setzero_si256()));
+	__m256i vmodeb = _mm256_add_epi64(vbitsb, _mm256_cmpgt_epi64(vbitsb, _mm256_setzero_si256()));
 
 	bits0 = _mm_cvtsi128_si32(_mm256_castsi256_si128(vbitsa));
 	bits1 = _mm_extract_epi32(_mm256_castsi256_si128(vbitsa), 2);
