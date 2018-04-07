76e9d31c865259c75afad0ff50dcf27151c43da6
UMEZAWA Takeshi <umezawa@utsoft.org>
2018-04-07 22:14:58 +0900

    ffmpeg が知らない FourCC の映像を読んだ時の警告ログが表示されないようにした。

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -21,4 +21,31 @@
 #define BOOST_TEST_MODULE tests-x64
 #endif
 
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
+
+extern "C"
+{
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4819)
+#endif
+#include <libavutil/log.h>
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
+}
+
+void av_log_custom_callback(void *avcl, int level, const char *fmt, va_list vl)
+{
+	if (level == AV_LOG_WARNING && strstr(fmt, "Could not find codec parameters") != NULL)
+		return;
+	av_log_default_callback(avcl, level, fmt, vl);
+}
+
+int main(int argc, char* argv[], char* envp[])
+{
+	av_log_set_callback(av_log_custom_callback);
+	return boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
+}
