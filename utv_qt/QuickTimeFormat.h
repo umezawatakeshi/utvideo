cd991b17b21b704514018712d80d399d4b7d56bb
UMEZAWA Takeshi <umezawa@utsoft.org>
2019-05-10 00:41:13 +0900

    mac版（QT版）のソースを削除した。

--- utv_qt/QuickTimeFormat.h
+++
@@ -1,7 +0,0 @@
-/* •¶šƒR[ƒh‚Í‚r‚i‚h‚r ‰üsƒR[ƒh‚Í‚b‚q‚k‚e */
-/* $Id$ */
-
-#pragma once
-
-int UtVideoFormatToQuickTimeFormat(OSType *pixelFormat, utvf_t utvf);
-int QuickTimeFormatToUtVideoFormat(utvf_t *utvf, OSType pixelFormat);
