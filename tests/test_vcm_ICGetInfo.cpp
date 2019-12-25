7b40347a3bbe25e4cec07ca3e1b5e619b392a511
UMEZAWA Takeshi <umezawa@utsoft.org>
2019-12-25 20:17:05 +0900

    VCM コーデックで temporal compression をサポートするかどうかのフラグを正しく返すようにした。

--- tests/test_vcm_ICGetInfo.cpp
+++ tests/test_vcm_ICGetInfo.cpp
@@ -4,7 +4,7 @@
 #include "stdafx.h"
 #include "test_win_fmt.h"
 
-BOOST_DATA_TEST_CASE(vcm_ICGetInfo, data::make(vecCodecFcc) ^ data::make(vecCodecShortName) ^ data::make(vecCodecLongName), fcc, wstrShortName, wstrLongName)
+BOOST_DATA_TEST_CASE(vcm_ICGetInfo, data::make(vecCodecFcc) ^ data::make(vecCodecShortName) ^ data::make(vecCodecLongName) ^ data::make(vecTemporalCompressionSupported), fcc, wstrShortName, wstrLongName, temporal)
 {
 	HIC hic;
 	LRESULT lr;
@@ -19,7 +19,10 @@ BOOST_DATA_TEST_CASE(vcm_ICGetInfo, data::make(vecCodecFcc) ^ data::make(vecCode
 	BOOST_CHECK_EQUAL(info.fccHandler, fcc);
 	BOOST_CHECK_EQUAL(wstring(info.szName), wstrShortName);
 	BOOST_CHECK_EQUAL(wstring(info.szDescription), wstrLongName);
-	BOOST_TEST_CHECK(info.dwFlags == 0);
+	if (temporal)
+		BOOST_TEST_CHECK(info.dwFlags == VIDCF_TEMPORAL);
+	else
+		BOOST_TEST_CHECK(info.dwFlags == 0);
 
 	lr = ICClose(hic);
 	BOOST_CHECK(lr == ICERR_OK);
