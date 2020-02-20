97de286f82b01ba97a2c0ebbc18c2b0032307ece
UMEZAWA Takeshi <umezawa@utsoft.org>
2020-02-21 05:23:41 +0900

    バイトオーダーをちゃんと検出するようにした。

--- utv_core/ByteOrder.h
+++ utv_core/ByteOrder.h
@@ -11,17 +11,23 @@ static inline uint16_t bswap16(uint16_t x) { return _byteswap_ushort(x); }
 static inline uint32_t bswap32(uint32_t x) { return _byteswap_ulong(x); }
 static inline uint64_t bswap64(uint64_t x) { return _byteswap_uint64(x); }
 
+#define LITTLE_ENDIAN 1234
+#define BIG_ENDIAN    4321
+#define PDP_ENDIAN    3412
+#define BYTE_ORDER    LITTLE_ENDIAN // 現在の Windows はリトルエンディアンの環境しかサポートしておらず、MSVC は Windows 用である。
+
 #elif defined(__GNUC__)
 
-/* gcc do not have __builtin_bswap16() */
 static inline uint16_t bswap16(uint16_t x) { return __builtin_bswap16(x); }
 static inline uint32_t bswap32(uint32_t x) { return __builtin_bswap32(x); }
 static inline uint64_t bswap64(uint64_t x) { return __builtin_bswap64(x); }
 
+#include <endian.h>
+
 #endif
 
 
-#if 1 /* XXX */
+#if BYTE_ORDER == LITTLE_ENDIAN
 
 /* little endian */
 
@@ -40,7 +46,7 @@ static inline uint64_t ltoh64(uint64_t x) { return x; }
 static inline uint64_t htob64(uint64_t x) { return bswap64(x); }
 static inline uint64_t btoh64(uint64_t x) { return bswap64(x); }
 
-#elif 0 /* XXX */
+#elif BYTE_ORDER == BIG_ENDIAN
 
 /* big endian */
 
