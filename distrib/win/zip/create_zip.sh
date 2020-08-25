754f07e65a0a66717a3fecf7b616fc033042aaf5
UMEZAWA Takeshi <umezawa@utsoft.org>
2020-08-25 23:00:43 +0900

    utv_logc.exe も zip ファイルにパッケージングするようにした。

--- distrib/win/zip/create_zip.sh
+++ distrib/win/zip/create_zip.sh
@@ -6,10 +6,14 @@ REPOROOT=../../..
 VERSION=$(cat ${REPOROOT}/utv_core/version.h | perl -ne '$_{$1}=$2 if /^\#define\s+UTVIDEO_VERSION_(\w+)\s+(\w+)/;END{print "$_{MAJOR}.$_{MINOR}.$_{BUGFIX}\n"}')
 
 TAG=utvideo-${VERSION}
-FNAME=${TAG}-win.zip
+ZIPFNAME=${TAG}-win.zip
+LOGFNAME=${TAG}-log-win.zip
 
-if [ -e "output/${FNAME}" ]; then
-    exec echo "output/${FNAME} already exists. Not overwritten."
+if [ -e "output/${ZIPFNAME}" ]; then
+    exec echo "output/${ZIPFNAME} already exists. Not overwritten."
+fi
+if [ -e "output/${LOGFNAME}" ]; then
+    exec echo "output/${LOGFNAME} already exists. Not overwritten."
 fi
 
 rm -rf zip/${TAG}
@@ -24,6 +28,11 @@ cp install.bat uninstall.bat globalconfig.bat zip/${TAG}/
 cp readme-zip.{en,ja}.txt zip/${TAG}/
 cp utvideo-{x86,x64}{,-unreg}.reg zip/${TAG}/
 
+rm -rf log/${TAG}-log
+mkdir -p log/${TAG}-log
+cp ${REPOROOT}/Release/utv_logc.exe log/${TAG}-log/
+cp ${REPOROOT}/readme.{en,ja}.html log/${TAG}-log/
+
 mkdir -p output
-cd zip
-exec powershell Compress-Archive -CompressionLevel Optimal -DestinationPath ../output/${FNAME} ${TAG}
+(cd zip; exec powershell Compress-Archive -CompressionLevel Optimal -DestinationPath ../output/${ZIPFNAME} ${TAG})
+(cd log; exec powershell Compress-Archive -CompressionLevel Optimal -DestinationPath ../output/${LOGFNAME} ${TAG}-log)
