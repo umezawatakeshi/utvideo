07ac47209e4a7ac7f1445b9cdcb7a32e2fe9dc9a
UMEZAWA Takeshi <umezawa@utsoft.org>
2021-04-13 20:50:54 +0900

    Ice Lake までの CODEFEATURE を定義した。

--- utv_core/TunedFunc_x86x64.h
+++ utv_core/TunedFunc_x86x64.h
@@ -3,10 +3,13 @@
 
 #pragma once
 
-//#define CODEFEATURE_SSE2  1
-//#define CODEFEATURE_SSE3  2
-//#define CODEFEATURE_SSSE3 3
-#define CODEFEATURE_SSE41 4
-#define CODEFEATURE_SSE42 5
-#define CODEFEATURE_AVX1  6
-#define CODEFEATURE_AVX2  7
+//#define CODEFEATURE_SSE2        1
+//#define CODEFEATURE_SSE3        2
+//#define CODEFEATURE_SSSE3       3
+#define CODEFEATURE_SSE41       4
+#define CODEFEATURE_SSE42       5
+#define CODEFEATURE_AVX1        6
+#define CODEFEATURE_AVX2        7
+#define CODEFEATURE_AVX512_SKX  8 // Skylake-X:         F, CD, DQ, BW, VL
+#define CODEFEATURE_AVX512_CNL  9 // Cannon Lake: SKX + IFMA, VBMI
+#define CODEFEATURE_AVX512_ICL 10 // Ice Lake:    CNL + VNNI, VPOPCNTDQ, VBMI2, BITALG
